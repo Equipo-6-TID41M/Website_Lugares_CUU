95c02a25901e95c134c4d8a17e3fe2e19fea9a13
Erickvoid <72955250+Erickvoid@users.noreply.github.com>
2020-12-02 16:43:15 -0700

    Actualizacion-API-Maps
    Se complemento el ultimo punto de nuestro sitio web.
    
    Co-Authored-By: Jassmin Ortiz <74668883+JassminOrtiz@users.noreply.github.com>

--- dist/getData.js
+++ dist/getData.js
@@ -11,15 +11,19 @@ const getConnection = () => {
             for (let index = 0; index < 2; index++) {
                 
                 itemHtml += `
-                <div class="col-sm-6">
-                <div class="card" style="width: 18rem;">
-                <img class="card-img-top"  src="${json.all_locations[index].imagen}" alt="${json.all_locations[index].nombre}">
-                <div class="card-body">
-                  <h5 class="card-title">${json.all_locations[index].nombre}</h5>
-                  <h3 class="card-text">${json.all_locations[index].telefono}</h3>
-                  <p class="card-text">${json.all_locations[index].ubicacion}</p>
-                </div>
-              </div> </div>`
+                <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="${json.all_locations[index].imagen}" alt="${json.all_locations[index].nombre}">
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="p-5">
+          <h5 class="card-title">${json.all_locations[index].nombre}</h5>
+          <h3 class="card-text">${json.all_locations[index].referencias}</h3>
+          <p class="card-text">${json.all_locations[index].ubicacion}</p>
+        </div></div></div></div>`                
             }
             itemHtml+=`</div> </div>`
     $("#portfolio .container .section-title").after(itemHtml);
